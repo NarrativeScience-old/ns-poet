b76a3fb5d0e03b834b7640c754dadfe4e336f462
Jonathan Drake <jdrake@narrativescience.com>
2021-11-11 12:06:30 -0800

    lint

--- ns_poet/package_targets/requirement.py
+++ ns_poet/package_targets/requirement.py
@@ -6,5 +6,11 @@ class PythonRequirement(BuildTarget):
     """Represents a Python requirement build target"""
 
     def __init__(self, package_name: str) -> None:
+        """Initializer
+
+        Args:
+            package_name: package name
+
+        """
         self.package_name = package_name
         self.key = f"{self.package_name}"
